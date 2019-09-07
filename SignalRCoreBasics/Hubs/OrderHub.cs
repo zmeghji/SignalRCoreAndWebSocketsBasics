01169e6201911707f155f167d6cd7dc499f4a825
Zeeshan Meghji <zeeshan_meghji@hotmail.com>
2019-09-07 17:08:23 -0400

    commit local copy

---
+++ SignalRCoreBasics/Hubs/OrderHub.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SignalRCoreBasics.Hubs
+{
+    public class OrderHub: Hub
+    {
+        public async Task GetUpdate()
+        {
+            for (int percentComplete = 0; percentComplete <= 100; percentComplete = percentComplete + 10)
+            {
+                Thread.Sleep(500);
+                await Clients.Caller.SendAsync("OrderProgressUpdate", percentComplete.ToString());
+            }
+            await Clients.Caller.SendAsync("Finished");
+
+        }
+    }
+}
